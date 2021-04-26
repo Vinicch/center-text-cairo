900777b03225e697539192e4be98f508cacd624a
Vinicch <vibalta@gmail.com>
2021-04-26 11:45:12 -0300

    feat: multiline text with cairo

--- Makefile
+++ Makefile
@@ -1,11 +1,5 @@
-cairo-build:
-	gcc -o watermark cairo.c $$(pkg-config --cflags --libs cairo)
+build:
+	gcc -o multiline_cairo cairo.c $$(pkg-config --cflags --libs cairo) && ./multiline_cairo
 
-cairo:
-	make cairo-build && ./watermark
-
-pango-build:
-	gcc -o watermark pango.c $$(pkg-config --cflags --libs cairo pangocairo)
-
-pango:
-	make pango-build && ./watermark
+run:
+	./multiline_cairo
--- cairo.c
+++ cairo.c
@@ -1,7 +1,50 @@
 #include <stdio.h>
 #include <cairo/cairo.h>
 
-int main(void)
+/**
+ * Calculates the width and height of all lines togheter.
+ * */
+void measure_block_size(cairo_t *cr, const char *lines[], int *width, int *height)
+{
+    for (int i = 0; i < 4; i++)
+    {
+        cairo_text_extents_t te;
+        cairo_text_extents(cr, lines[i], &te);
+
+        if (te.width > *width)
+            *width = te.width;
+
+        *height = *height + te.height + 10;
+    }
+}
+
+/**
+ * Writes all lines into the cairo surface.
+ * */
+void write_multiline(cairo_t *cr, const char *lines[], int width, int height)
+{
+    int block_width = 0, block_height = 0, line_height = 0;
+
+    measure_block_size(cr, lines, &block_width, &block_height);
+
+    for (int i = 0; i < 4; i++)
+    {
+        // Adjust positions
+        cairo_text_extents_t te;
+        cairo_text_extents(cr, lines[i], &te);
+
+        double x = (width / 2 - block_width / 2 - te.x_bearing);
+        double y = (height / 2 - block_height / 2 + line_height);
+
+        // Move to center and show text
+        cairo_move_to(cr, x, y);
+        cairo_show_text(cr, lines[i]);
+
+        line_height = line_height + te.height + 10;
+    }
+}
+
+int main()
 {
     cairo_surface_t *surface;
     cairo_t *cr;
@@ -9,27 +52,24 @@ int main(void)
     int width = 1280;
     int height = 720;
 
-    const char *message = "Hello World";
+    const char *lines[] = {"Vinicch",
+                           "Hello World",
+                           "Machine User",
+                           "Date"};
 
-    // Create canvas
+    // Create context
     surface = cairo_image_surface_create(CAIRO_FORMAT_ARGB32, width, height);
     cr = cairo_create(surface);
 
-    // Config font settings
+    // Config source settings
     cairo_set_source_rgb(cr, 255, 255, 255);
+
+    // Config mask settings
     cairo_select_font_face(cr, "Sans", CAIRO_FONT_SLANT_NORMAL, CAIRO_FONT_WEIGHT_NORMAL);
     cairo_set_font_size(cr, 40.0);
 
-    // Adjust positions
-    cairo_text_extents_t te;
-    cairo_text_extents(cr, message, &te);
-
-    double x = (width / 2 - te.width / 2 - te.x_bearing);
-    double y = (height / 2 + te.height / 2);
-
-    // Move to center and show text
-    cairo_move_to(cr, x, y);
-    cairo_show_text(cr, message);
+    // Write watermark text on surface
+    write_multiline(cr, lines, width, height);
 
     // Write to image, close and exit
     cairo_surface_write_to_png(surface, "image.png");
